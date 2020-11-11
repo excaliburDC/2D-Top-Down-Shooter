66b637cf80f6e63e7b1da99c54c0a4336d986dc3
DC <43692686+excaliburDC@users.noreply.github.com>
2020-11-11 17:51:38 +0530

    minor bug fixes regarding scrolling

--- Assets/Scripts/ScrollingBG.cs
+++ Assets/Scripts/ScrollingBG.cs
@@ -3,17 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+
 public class ScrollingBG : MonoBehaviour
 {
-    [SerializeField] private float scrollSpeed = 2f;
+    [SerializeField] private float scrollSpeed = 0.1f;
 
-    private Rigidbody2D rb2D;
+    private MeshRenderer meshRenderer;
+
+    private float yScrollSpeed;
 
  
     void Awake()
     {
-        rb2D = GetComponent<Rigidbody2D>();
+        meshRenderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -27,6 +29,10 @@ public class ScrollingBG : MonoBehaviour
     /// </summary>
     private void BeginScrolling()
     {
-        rb2D.velocity = new Vector2(0f, -scrollSpeed);
+        yScrollSpeed = Time.time * scrollSpeed;
+
+        Vector2 offset = new Vector2(0f, yScrollSpeed);
+
+        meshRenderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
     }
 }
